2aca0f3db26cca6ae9fc113d46a1eb8630f2fe88
Graham Fairweather <xotic750@gmail.com>
2019-08-20 20:20:32 +0200

    Maintenance

--- dist/define-properties-x.js
+++ dist/define-properties-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-2017",
-  "date": "2019-08-15T20:54:19.909Z",
+  "date": "2019-08-20T18:20:24.640Z",
   "describe": "",
   "description": "Define multiple non-enumerable properties at once.",
   "file": "define-properties-x.js",
